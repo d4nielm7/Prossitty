1e52b5bbca80e1aed627a95d58e9e556b8cd127e
DANIEL <d4nielm7@gmail.com>
2024-10-10 13:32:04 +0200

    p

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {}
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     logging: {
         fetches: {
             fullUrl: true,
         },
     },
-}
+    assetPrefix: isProd ? '/Prossity/' : '', // Adjust for your GitHub Pages repo name
+    basePath: isProd ? '/Prossity' : '',    // Adjust for your GitHub Pages repo name
+    trailingSlash: true,                    // Ensures that static assets are correctly referenced
+    images: {
+        unoptimized: true,                  // GitHub Pages doesn't support image optimization
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
